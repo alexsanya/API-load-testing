cf2ce8bae90038b4a66207317a32f0629c6fd7af
alexander <kovalas@yandex.ru>
2016-08-18 19:22:24 +0300

    clean garbadge

--- Procfile
+++
@@ -1,3 +0,0 @@
-master: babel-node master.js --presets es2015,stage-2
-workerCreator: babel-node workerCreator.js --presets es2015,stage-2
-workerActivity: babel-node workerActivity.js --presets es2015,stage-2
--- utils/testWorker.js
+++
@@ -1,9 +0,0 @@
-import logger from './node_modules/logfmt';
-import getMessageQueue from './messageQueue';
-
-logger.log({ msg: 'Test worker started..' });
-
-getMessageQueue().push('createCompany', {
-  numOfUsers: 3,
-});
-
