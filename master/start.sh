be359f781eca0bd1459f75bf7310a73390fbe41f
alexander <kovalas@yandex.ru>
2016-08-19 16:11:25 +0300

    fix master launch script

--- master/start.sh
+++ master/start.sh
@@ -1,20 +1,19 @@
 #! /bin/sh
-if [ ! -d "node_modules" ]; then
-  echo "Installing dependencies..."
-  npm install
-fi
-
 if [ ! -d "build" ]; then 
   echo "Compiling code..."
-  ./node_modules/.bin/babel --presets es2015,stage-2 -d build/ .
+  babel --presets es2015,stage-2 -d build/ .
 fi
 
 if [ ! -d "lib" ]; then
   echo "Compiling libraries..."
-  ./node_modules/.bin/babel --presets es2015,stage-2 -d lib/ ../lib
+  babel --presets es2015,stage-2 -d lib/ ../lib/
 fi
 
+cp package.json build/
 cd build
+if [ ! -d "node_modules" ]; then
+  echo "Installing dependencies..."
+  npm install
+fi
 echo "Starting process..."
 npm start
-
